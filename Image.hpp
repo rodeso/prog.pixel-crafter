7a57d334c0f12fbd345dc96217bc6856f251b086
gcl <trabalhosko541@gmail.com>
2023-04-26 17:45:08 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -1,12 +1,18 @@
 #ifndef __prog_Image_hpp__
 #define __prog_Image_hpp__
 #include "Color.hpp"
+#include <vector>
 
 namespace prog
 {
   class Image
   {
   private:
+    int width_;
+    int height_;
+    std::vector<std::vector<Color>>m_pixels;
+
+};
     // TODO: define private fields for image state
   public:
     Image(int w, int h, const Color &fill = {255, 255, 255});
@@ -16,5 +22,5 @@ namespace prog
     Color &at(int x, int y);
     const Color &at(int x, int y) const;
   };
-}
+
 #endif
