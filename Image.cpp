b2b39ae9ff834fa2acfc7fcf189b55141e5bf957
RafaelCampeao <127619961+RafaelCampeao@users.noreply.github.com>
2023-04-27 13:11:13 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -1,30 +1,47 @@
 #include "Image.hpp"
-
+#include <vector>
 namespace prog
 {
-  Image::Image(int w, int h, const Color &fill) //2D Array or Vector to implement object pixel as color
+  Image::Image(int w, int h, const Color &fill)
   {
+    w_=w;
+    h_=h;
+    
+    for (int i = 0; i < w_; i++)
+    {
+      std::vector<Color> v;
+      for(int j =0 ; j<h_ ;j++){
+        v.push_back(fill);
+      }
+      pixeis_.push_back(v);
+      
+    }
+    
+    
   }
   Image::~Image()
   {
+    
   }
   int Image::width() const
   {
-    return width_;
+    return w_;
   }
   int Image::height() const
   {
-    return height_;
+    return h_;
   }
 
+  
+  
 
   Color& Image::at(int x, int y)
   {
-    return pixel_[x][y];
+    return pixeis_[x][y];
   }
 
   const Color& Image::at(int x, int y) const
   {
-    return pixel_[x][y];
+    return pixeis_[x][y];
   }
 }
