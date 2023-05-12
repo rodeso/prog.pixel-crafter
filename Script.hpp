198a4b2e1e903843e2c2bf5736e32add5adf4f04
Rodrigo de Sousa <93033013+Rodri-5@users.noreply.github.com>
2023-05-12 01:21:50 +0100

    Update Script.hpp

--- Script.hpp
+++ Script.hpp
@@ -24,14 +24,13 @@ namespace prog
     void open();
     void blank();
     void save();
-    void run();
     void invert();
     void to_gray_scale();
     void replace(Color c1 ,Color c2);
     void fill(int x, int y, int w, int h, Color c);
     void v_mirror();
     void h_mirror();
-    void add(const std::string &filename, rgb_value r, rgb_value g, rgb_value b, int x, int y);
+    void add();
     void crop(int x, int y, int w, int h);
     void rotate_left();
     void rotate_rigth();
